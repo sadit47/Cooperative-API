86268e53cf2aa8ba6ce60b9e3b1bfcf786f60135
sadit47 <saditmeelek67@gmail.com>
2026-05-04 18:39:00 +0700

    Add role middleware

---
+++ app/Http/Middleware/RoleMiddleware.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class RoleMiddleware
+{
+    public function handle(Request $request, Closure $next, string $role)
+    {
+        if (!$request->user() || $request->user()->role !== $role) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Forbidden',
+                'errors' => [
+                    'role' => ['You do not have permission to access this resource.']
+                ],
+            ], 403);
+        }
+
+        return $next($request);
+    }
+}

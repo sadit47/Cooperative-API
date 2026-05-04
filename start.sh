07eb3065c4d40fb1eac1b28d0ff9304409f420e4
sadit47 <saditmeelek67@gmail.com>
2026-05-05 02:59:12 +0700

    Fix Apache MPM at runtime

--- start.sh
+++ start.sh
@@ -3,4 +3,9 @@
 sed -i "s/Listen 80/Listen ${PORT:-80}/g" /etc/apache2/ports.conf
 sed -i "s/<VirtualHost \*:80>/<VirtualHost *:${PORT:-80}>/g" /etc/apache2/sites-available/000-default.conf
 
+# 🔥 FIX MPM ตอน runtime (สำคัญสุด)
+a2dismod mpm_event || true
+a2dismod mpm_worker || true
+a2enmod mpm_prefork
+
 apache2-foreground
